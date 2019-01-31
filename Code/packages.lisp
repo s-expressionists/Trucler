e4ce4145fcabc94823e8028eacc97aa0a04f2505
Robert Strandh <robert.strandh@gmail.com>
2019-01-31 14:59:38 +0100

    Export names of condition types, and comment other exported symbols.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -9,18 +9,31 @@
            #:compiler-macro
            #:dynamic-extent)
   (:export #:info
+           ;; Generic function and base class for variable information.
            #:variable-info
+           ;; Classes for variable information.
            #:lexical-variable-info
            #:special-variable-info
            #:constant-variable-info
            #:symbol-macro-info
+           ;; Generic function and base class for function information.
            #:function-info
+           ;; Classes for function information.
            #:local-function-info
            #:global-function-info
            #:generic-function-info
            #:local-macro-info
            #:global-macro-info
            #:special-operator-info
+           ;; Generic function and class for block information.
            #:block-info
+           ;; Generic function and class for tag information.
            #:tag-info
-           #:optimize-info))
+           ;; Generic function and class for optimize information.
+           #:optimize-info
+           ;; Condition types
+           #:no-info
+           #:no-variable-info
+           #:no-function-info
+           #:no-block-info
+           #:no-tag-info))
