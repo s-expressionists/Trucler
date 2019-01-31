89191603577537f80784eb18d8e8b3d904ab732a
Robert Strandh <robert.strandh@gmail.com>
2019-01-31 15:15:11 +0100

    Remove comments.  This information is now in documentation strings.

--- Code/query-functions.lisp
+++ Code/query-functions.lisp
@@ -11,48 +11,12 @@
 ;;;; used on those standard objects to determine the required
 ;;;; information.
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; VARIABLE-INFO.
-
-;;; Return an instance of the class VARIABLE-INFO describing the role
-;;; of SYMBOL in ENVIRONMENT when SYMBOL was found in a variable
-;;; position.
 (defgeneric variable-info (environment symbol))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; FUNCTION-INFO.
-
-;;; Return an instance of the class FUNCTION-INFO describing the role
-;;; of FUNCTION-NAME in ENVIRONMENT when FUNCTION-NAME was found in a
-;;; function position.  This function can also be used to obtain
-;;; information about a function name that is not a symbol.
 (defgeneric function-info (environment function-name))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; BLOCK-INFO.
-
-;;; Return an instance of the class BLOCK-INFO describing the role of
-;;; SYMBOL in ENVIRONMENT when SYMBOL was found in a position to name
-;;; a block.
 (defgeneric block-info (environment symbol))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; TAG-INFO.
-
-;;; Return an instance of the class TAG-INFO describing the role of
-;;; TAG in ENVIRONMENT when TAG was found in a position to name a
-;;; TAGBODY tag, i.e. as an argument to GO.  TAG must be a symbol or
-;;; an integer.
 (defgeneric tag-info (environment tag))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; OPTIMIZE-INFO.
-
-;;; Return an instance of the class OPTIMIZE-INFO describing the
-;;; values of all OPTIMIZE qualities in ENVIRONMENT.
 (defgeneric optimize-info (environment))
