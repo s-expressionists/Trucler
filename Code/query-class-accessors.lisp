b69d0dc79628546b13cf80fe8d6787d31e931c85
Robert Strandh <robert.strandh@gmail.com>
2019-01-31 17:31:25 +0100

    Supply DEFGENERIC forms for the remaining accessor functions.

--- Code/query-class-accessors.lisp
+++ Code/query-class-accessors.lisp
@@ -3,3 +3,27 @@
 (defgeneric name (info-instance))
 
 (defgeneric identity (info-instance))
+
+(defgeneric type (info-instance))
+
+(defgeneric inline (info-instance))
+
+(defgeneric ignore (info-instance))
+
+(defgeneric dynamic-extent (info-instance))
+
+(defgeneric ast (info-instance))
+
+(defgeneric compiler-macro (info-instance))
+
+(defgeneric expander (info-instance))
+
+(defgeneric global-p (info-instance))
+
+(defgeneric value (info-instance))
+
+(defgeneric expansion (info-instance))
+
+(defgeneric class-name (info-instance))
+
+(defgeneric method-class-name (info-instance))
