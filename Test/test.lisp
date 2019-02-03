4d03721f39215e5080905dac1341a1fb977a9e80
Robert Strandh <robert.strandh@gmail.com>
2019-02-03 15:21:14 +0100

    Test ADD-BLOCK and BLOCK-INFO.

--- Test/test.lisp
+++ Test/test.lisp
@@ -1,4 +1,19 @@
 (cl:in-package #:trucler-test)
 
+(defun test-block ()
+  (let* ((global-environment (make-instance 'environment))
+         (client (make-instance 'trucler-test-client))
+         (e1 (make-instance 'trucler::environment
+               :global-environment global-environment))
+         (e2 (trucler:add-block client e1 'hello))
+         (e3 (trucler:add-block client e2 'hello))
+         (b1 (trucler:block-info client e2 'hello))
+         (b2 (trucler:block-info client e3 'hello)))
+    (assert (not (null b1)))
+    (assert (not (null b2)))
+    (assert (not (eq b1 b2)))
+    (assert (eq (trucler:name b1) 'hello))
+    (assert (eq (trucler:name b2) 'hello))))
+
 (defun test ()
-  nil)
+  (test-block))
