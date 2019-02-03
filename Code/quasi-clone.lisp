bb891355ede9feb0da5736b8d193a35bff73556f
Robert Strandh <robert.strandh@gmail.com>
2019-02-03 14:57:17 +0100

    Supply the class to MAKE-INSTANCE.

--- Code/quasi-clone.lisp
+++ Code/quasi-clone.lisp
@@ -2,6 +2,7 @@
 
 (defun quasi-clone (instance initarg initval)
   (apply #'make-instance
+         (class-of instance)
          initarg initval
          (loop for (initarg reader) in (clone-info instance)
                collect initarg
