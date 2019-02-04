c7397674d4896ca16aa3b67462c59ece927e9c5e
Robert Strandh <robert.strandh@gmail.com>
2019-02-04 10:33:50 +0100

    Check that a deep tag is found.

--- Test/test.lisp
+++ Test/test.lisp
@@ -22,15 +22,19 @@
 (defun test-tag ()
   (let* ((global-environment (make-instance 'environment))
          (client (make-instance 'trucler-test-client))
-         (e1 (make-instance 'trucler::environment
+         (e0 (make-instance 'trucler::environment
                :global-environment global-environment))
+         (e1 (trucler:add-tag client e0 'foo))
          (e2 (trucler:add-tag client e1 'hello))
          (e3 (trucler:add-tag client e2 'hello))
+         (b0 (trucler:tag-info client e3 'foo))
          (b1 (trucler:tag-info client e2 'hello))
          (b2 (trucler:tag-info client e3 'hello)))
+    (assert (not (null b0)))
     (assert (not (null b1)))
     (assert (not (null b2)))
     (assert (not (eq b1 b2)))
+    (assert (eq (trucler:name b0) 'foo))
     (assert (eq (trucler:name b1) 'hello))
     (assert (eq (trucler:name b2) 'hello))))
 
