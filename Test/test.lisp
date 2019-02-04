5124600f441e9c980987e729594a968e7c47cfaa
Robert Strandh <robert.strandh@gmail.com>
2019-02-04 10:31:53 +0100

    Check that a deep block is found.

--- Test/test.lisp
+++ Test/test.lisp
@@ -3,15 +3,19 @@
 (defun test-block ()
   (let* ((global-environment (make-instance 'environment))
          (client (make-instance 'trucler-test-client))
-         (e1 (make-instance 'trucler::environment
+         (e0 (make-instance 'trucler::environment
                :global-environment global-environment))
-         (e2 (trucler:add-block client e1 'hello))
+	 (e1 (trucler:add-block client e0 'foo))
+	 (e2 (trucler:add-block client e1 'hello))
          (e3 (trucler:add-block client e2 'hello))
+	 (b0 (trucler:block-info client e3 'foo))
          (b1 (trucler:block-info client e2 'hello))
          (b2 (trucler:block-info client e3 'hello)))
+    (assert (not (null b0)))
     (assert (not (null b1)))
     (assert (not (null b2)))
     (assert (not (eq b1 b2)))
+    (assert (eq (trucler:name b0) 'foo))
     (assert (eq (trucler:name b1) 'hello))
     (assert (eq (trucler:name b2) 'hello))))
 
