a0eb6bb09d86813f74be3d949ba34aaae4d0a4e6
Robert Strandh <robert.strandh@gmail.com>
2019-02-17 08:26:47 +0100

    Use keyword parameters for macro to allow :INITFORM and :TYPE slot options.

--- Code/query-mixin-classes.lisp
+++ Code/query-mixin-classes.lisp
@@ -1,15 +1,16 @@
 (cl:in-package #:trucler)
 
-(defmacro define-mixin-class (class-name
-                              slot-name
-                              initarg
-                              reader-name
-                              &optional (initform nil initform-p))
+(defmacro define-mixin-class
+    (class-name
+     slot-name
+     initarg
+     reader-name
+     &key
+       (initform `(error "Initarg ~s must be given." ,initarg))
+       (type t))
   `(progn (defclass ,class-name ()
-            ((,slot-name :initform
-                         ,(if initform-p
-                              initform
-                              `(error "Initarg ~s must be given." ,initarg))
+            ((,slot-name :initform ,initform
+                         :type ,type
                          :initarg ,initarg
                          :reader ,reader-name)))
 
@@ -20,18 +21,26 @@
 
 (define-mixin-class identity-mixin %identity :identity identity)
 
-(define-mixin-class type-mixin %type :type type t)
+(define-mixin-class type-mixin %type :type type
+    :initform t)
 
-(define-mixin-class ignore-mixin %ignore :ignore ignore nil)
+(define-mixin-class ignore-mixin %ignore :ignore ignore
+    :initform nil)
 
 (define-mixin-class dynamic-extent-mixin
-    %dynamic-extent :dynamic-extent dynamic-extent nil)
+    %dynamic-extent :dynamic-extent dynamic-extent
+    :initform nil)
 
 (define-mixin-class compiler-macro-mixin
-    %compiler-macro :compiler-macro compiler-macro nil)
+    %compiler-macro :compiler-macro compiler-macro
+    :initform nil)
 
 (define-mixin-class expansion-mixin %expansion :expansion expansion)
 
 (define-mixin-class expander-mixin %expander :expander expander)
 
 (define-mixin-class class-name-mixin %class-name :class-name class-name)
+
+(define-mixin-class inline-mixin %inline :inline inline
+    :initform nil
+    :type (member nil cl:inline cl:notinline))
