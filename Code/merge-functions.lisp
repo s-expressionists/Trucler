3e3bbd1d49fbff799763be21150c562faa2f505e
Robert Strandh <robert.strandh@gmail.com>
2019-02-19 17:22:12 +0100

    Add CLIENT parameter to MERGE- functions.

--- Code/merge-functions.lisp
+++ Code/merge-functions.lisp
@@ -1,25 +1,25 @@
 (cl:in-package #:trucler)
 
-(defgeneric merge-variable-type (variable-information type))
+(defgeneric merge-variable-type (client variable-information type))
 
-(defgeneric merge-variable-ignore (variable-information ignore))
+(defgeneric merge-variable-ignore (client variable-information ignore))
 
-(defgeneric merge-variable-dynamic-extent (variable-information))
+(defgeneric merge-variable-dynamic-extent (client variable-information))
 
-(defgeneric merge-function-type (function-information type))
+(defgeneric merge-function-type (client function-information type))
 
-(defgeneric merge-function-ignore (function-information ignore))
+(defgeneric merge-function-ignore (client function-information ignore))
 
-(defgeneric merge-function-dynamic-extent (function-information))
+(defgeneric merge-function-dynamic-extent (client function-information))
 
-(defgeneric merge-inline (function-information inline))
+(defgeneric merge-inline (client function-information inline))
 
-(defgeneric merge-speed (optimize-information value))
+(defgeneric merge-speed (client optimize-information value))
 
-(defgeneric merge-compilation-speed (optimize-information value))
+(defgeneric merge-compilation-speed (client optimize-information value))
 
-(defgeneric merge-debug (optimize-information value))
+(defgeneric merge-debug (client optimize-information value))
 
-(defgeneric merge-space (optimize-information value))
+(defgeneric merge-space (client optimize-information value))
 
-(defgeneric merge-safety (optimize-information value))
+(defgeneric merge-safety (client optimize-information value))
--- Code/merge-methods.lisp
+++ Code/merge-methods.lisp
@@ -1,37 +1,37 @@
 (cl:in-package #:trucler)
 
-(defmethod merge-variable-type ((info type-mixin) type)
+(defmethod merge-variable-type (client (info type-mixin) type)
   (quasi-clone info :type `(and type ,(type info))))
 
-(defmethod merge-function-type ((info type-mixin) type)
+(defmethod merge-function-type (client (info type-mixin) type)
   (quasi-clone info :type `(and type ,(type info))))
 
-(defmethod merge-variable-ignore ((info ignore-mixin) ignore)
+(defmethod merge-variable-ignore (client (info ignore-mixin) ignore)
   (quasi-clone info :ignore ignore))
 
-(defmethod merge-function-ignore ((info ignore-mixin) ignore)
+(defmethod merge-function-ignore (client (info ignore-mixin) ignore)
   (quasi-clone info :ignore ignore))
 
-(defmethod merge-variable-dynamic-extent ((info dynamic-extent-mixin))
+(defmethod merge-variable-dynamic-extent (client (info dynamic-extent-mixin))
   (quasi-clone info :dynamic-extent t))
 
-(defmethod merge-function-dynamic-extent ((info dynamic-extent-mixin))
+(defmethod merge-function-dynamic-extent (client (info dynamic-extent-mixin))
   (quasi-clone info :dynamic-extent t))
 
-(defmethod merge-inline ((info function-information) inline)
+(defmethod merge-inline (client (info function-information) inline)
   (quasi-clone info :inline inline))
 
-(defmethod merge-speed ((info optimize-information) value)
+(defmethod merge-speed (client (info optimize-information) value)
   (quasi-clone info :speed value))
 
-(defmethod merge-compilation-speed ((info optimize-information) value)
+(defmethod merge-compilation-speed (client (info optimize-information) value)
   (quasi-clone info :compilation-speed value))
 
-(defmethod merge-debug ((info optimize-information) value)
+(defmethod merge-debug (client (info optimize-information) value)
   (quasi-clone info :debug value))
 
-(defmethod merge-safety ((info optimize-information) value)
+(defmethod merge-safety (client (info optimize-information) value)
   (quasi-clone info :safety value))
 
-(defmethod merge-space ((info optimize-information) value)
+(defmethod merge-space (client (info optimize-information) value)
   (quasi-clone info :space value))
