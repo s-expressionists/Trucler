1f1103175bd34ea8c5bb883dbfd66e9ffd5307dc
Robert Strandh <robert.strandh@gmail.com>
2019-02-20 10:26:48 +0100

    Rename parameter to better reflect its purpose.

--- Code/merge-functions.lisp
+++ Code/merge-functions.lisp
@@ -1,19 +1,19 @@
 (cl:in-package #:trucler)
 
-(defgeneric merge-type (client variable-information type))
+(defgeneric merge-type (client information type))
 
-(defgeneric merge-ignore (client variable-information ignore))
+(defgeneric merge-ignore (client information ignore))
 
-(defgeneric merge-dynamic-extent (client variable-information))
+(defgeneric merge-dynamic-extent (client information))
 
-(defgeneric merge-inline (client function-information inline))
+(defgeneric merge-inline (client information inline))
 
-(defgeneric merge-speed (client optimize-information value))
+(defgeneric merge-speed (client information value))
 
-(defgeneric merge-compilation-speed (client optimize-information value))
+(defgeneric merge-compilation-speed (client information value))
 
-(defgeneric merge-debug (client optimize-information value))
+(defgeneric merge-debug (client information value))
 
-(defgeneric merge-space (client optimize-information value))
+(defgeneric merge-space (client information value))
 
-(defgeneric merge-safety (client optimize-information value))
+(defgeneric merge-safety (client information value))
