48df21a55263e1949efb16d19ff1105bdf999f11
Robert Strandh <robert.strandh@gmail.com>
2019-02-21 09:24:22 +0100

    Define MERGE-? methods for error situations.

--- Code/merge-methods.lisp
+++ Code/merge-methods.lisp
@@ -1,5 +1,54 @@
 (cl:in-package #:trucler)
 
+;;; Methods for catching error situations.
+
+(defmethod merge-type (client description type)
+  (declare (cl:ignore client type))
+  (error 'invalid-description-for-merging-type-information
+         :description description))
+
+(defmethod merge-ignore (client description ignore)
+  (declare (cl:ignore client ignore))
+  (error 'invalid-description-for-merging-ignore-information
+         :description description))
+
+(defmethod merge-dynamic-extent (client description)
+  (declare (cl:ignore client))
+  (error 'invalid-description-for-merging-dynamic-extent-information
+         :description description))
+
+(defmethod merge-inline (client description inline)
+  (declare (cl:ignore client inline))
+  (error 'invalid-description-for-merging-inline-information
+         :description description))
+
+(defmethod merge-speed (client description value)
+  (declare (cl:ignore client value))
+  (error 'invalid-description-for-merging-speed-information
+         :description description))
+
+(defmethod merge-compilation-speed (client description value)
+  (declare (cl:ignore client value))
+  (error 'invalid-description-for-merging-compilation-speed-information
+         :description description))
+
+(defmethod merge-debug (client description value)
+  (declare (cl:ignore client value))
+  (error 'invalid-description-for-merging-debug-information
+         :description description))
+
+(defmethod merge-safety (client description value)
+  (declare (cl:ignore client value))
+  (error 'invalid-description-for-merging-safety-information
+         :description description))
+
+(defmethod merge-space (client description value)
+  (declare (cl:ignore client value))
+  (error 'invalid-description-for-merging-space-information
+         :description description))
+
+;;; Main methods.
+
 (defmethod merge-type (client (description type-mixin) type)
   (quasi-clone description :type `(and type ,(type description))))
 
