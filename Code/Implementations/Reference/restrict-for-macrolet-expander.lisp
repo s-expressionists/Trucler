ea456ff0540182944e70307d6087f2311e79e570
Robert Strandh <robert.strandh@gmail.com>
2019-03-20 17:35:49 +0100

    Specialize the environment parameter.

--- Code/Implementations/Reference/restrict-for-macrolet-expander.lisp
+++ Code/Implementations/Reference/restrict-for-macrolet-expander.lisp
@@ -29,7 +29,7 @@
 (defun restrict-descriptions (descriptions)
   (mapcar #'restrict-description descriptions))
 
-(defmethod trucler:restrict-for-macrolet-expander (client environment)
+(defmethod trucler:restrict-for-macrolet-expander (client (environment environment))
   (apply #'make-instance
          (class-of environment)
          :function-description
