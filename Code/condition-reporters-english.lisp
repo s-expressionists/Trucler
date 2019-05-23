5c207cdaeb868eff85f94b88f456136a56b3c35d
Robert Strandh <robert.strandh@gmail.com>
2019-05-23 15:56:09 +0200

    Fix incorrect condition reporters.

--- Code/condition-reporters-english.lisp
+++ Code/condition-reporters-english.lisp
@@ -25,60 +25,69 @@
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging type information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-ignore-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging ignore information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-dynamic-extent-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging dynamic-extent information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-inline-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging inline information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-speed-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging speed information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-compilation-speed-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging compilation-speed information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-debug-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging debug information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-safety-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging safety information ~s"
+            (description condition))))
 
 (defmethod acclimation:report-condition
     ((condition invalid-description-for-merging-space-information)
      stream
      (language acclimation:english))
   (let ((*package* (find-package '#:keyword)))
-    (format stream "Undefined tag named ~s" (description condition))))
+    (format stream "Invalid description for merging space information ~s"
+            (description condition))))
