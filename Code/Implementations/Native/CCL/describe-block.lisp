6acdaf7eecb823da1ed348ed1c06f32efe79e132
Marco Heisig <marco.heisig@fau.de>
2019-10-20 18:00:55 +0200

    Implement DESCRIBE-BLOCK on CCL.

--- Code/Implementations/Native/CCL/describe-block.lisp
+++ Code/Implementations/Native/CCL/describe-block.lisp
@@ -3,3 +3,19 @@
 (defmethod trucler:describe-block
     ((client client) (env null) (name symbol))
   (trucler:describe-block client *null-lexical-environment* name))
+
+(defmethod trucler:describe-block
+    ((client client) (env ccl::lexical-environment) (name symbol))
+  (describe-block name))
+
+(defmethod trucler:describe-block
+    ((client client) (env ccl::definition-environment) (name symbol))
+  (describe-block name))
+
+(defun describe-block (name)
+  (let ((info (ccl::nx-block-info name)))
+    (if (not info)
+        nil
+        (make-instance 'block-description
+          :name name
+          :identity info))))
