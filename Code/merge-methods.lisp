425d8286714964550bc30530d53499efe6ae3ac2
Paul A. Patience <paul@apatience.com>
2023-09-02 17:03:52 -0400

    Ignore client arguments where appropriate.

--- Code/merge-methods.lisp
+++ Code/merge-methods.lisp
@@ -55,31 +55,41 @@
 ;;; Main methods.
 
 (defmethod merge-type (client (description type-mixin) type)
+  (declare (cl:ignore client))
   (quasi-clone description :type `(and type ,(type description))))
 
 (defmethod merge-ignore (client (description ignore-mixin) ignore)
+  (declare (cl:ignore client))
   (quasi-clone description :ignore ignore))
 
 (defmethod merge-dynamic-extent (client (description dynamic-extent-mixin))
+  (declare (cl:ignore client))
   (quasi-clone description :dynamic-extent t))
 
 (defmethod merge-inline (client (description inline-mixin) inline)
+  (declare (cl:ignore client))
   (quasi-clone description :inline inline))
 
 (defmethod merge-inline-data (client (description inline-data-mixin) inline-data)
+  (declare (cl:ignore client))
   (quasi-clone description :inline-data inline-data))
 
 (defmethod merge-speed (client (description speed-mixin) value)
+  (declare (cl:ignore client))
   (quasi-clone description :speed value))
 
 (defmethod merge-compilation-speed (client (description compilation-speed-mixin) value)
+  (declare (cl:ignore client))
   (quasi-clone description :compilation-speed value))
 
 (defmethod merge-debug (client (description debug-mixin) value)
+  (declare (cl:ignore client))
   (quasi-clone description :debug value))
 
 (defmethod merge-safety (client (description safety-mixin) value)
+  (declare (cl:ignore client))
   (quasi-clone description :safety value))
 
 (defmethod merge-space (client (description space-mixin) value)
+  (declare (cl:ignore client))
   (quasi-clone description :space value))
--- Code/query-factories.lisp
+++ Code/query-factories.lisp
@@ -2,28 +2,35 @@
 
 (defgeneric lexical-variable-description-class (client)
   (:method (client)
+    (declare (cl:ignore client))
     (find-class 'lexical-variable-description)))
 
 (defgeneric local-special-variable-description-class (client)
   (:method (client)
+    (declare (cl:ignore client))
     (find-class 'local-special-variable-description)))
 
 (defgeneric local-symbol-macro-description-class (client)
   (:method (client)
+    (declare (cl:ignore client))
     (find-class 'local-symbol-macro-description)))
 
 (defgeneric local-function-description-class (client)
   (:method (client)
+    (declare (cl:ignore client))
     (find-class 'local-function-description)))
 
 (defgeneric local-macro-description-class (client)
   (:method (client)
+    (declare (cl:ignore client))
     (find-class 'local-macro-description)))
 
 (defgeneric block-description-class (client)
   (:method (client)
+    (declare (cl:ignore client))
     (find-class 'block-description)))
 
 (defgeneric tag-description-class (client)
   (:method (client)
+    (declare (cl:ignore client))
     (find-class 'tag-description)))
